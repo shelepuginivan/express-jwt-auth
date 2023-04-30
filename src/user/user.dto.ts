8be869779fca16bb4002ed1633ec8aa595ffdbf4
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-30 19:46:34 +0300

    Remove password field from constructor object

--- src/user/user.dto.ts
+++ src/user/user.dto.ts
@@ -5,7 +5,7 @@ export class UserDto {
 	email: string
 	role: Role
 
-	constructor(user: User) {
+	constructor(user: Omit<User, 'password'>) {
 		this.username = user.username
 		this.email = user.email
 		this.role = user.role
