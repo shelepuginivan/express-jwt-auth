7906f9992cf36973b6c72bc9b3ea8e935bd2d50d
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-30 17:45:25 +0300

    Added server startup function

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { config } from 'dotenv'
+import express from 'express'
+import { connect } from 'mongoose'
+import { join } from 'path'
+
+config({
+	path: join(__dirname, '..', '.env')
+})
+
+const app = express()
+
+const start = async () => {
+	const port = Number(process.env.PORT) || 8000
+	const mongoUri = process.env.MONGO_URI
+
+	if (!mongoUri) {
+		throw Error('Failed to start app - MongoDB URI were not specified')
+	}
+
+	await connect(mongoUri)
+	app.listen(port)
+}
+
+start().catch(error => console.log(error))
+
