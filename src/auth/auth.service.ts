d732cf5ce2e74616357e3cd100097d261dcda976
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-30 19:21:32 +0300

    Hash user password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { Role, User } from '../user/user'
 import { UserDto } from '../user/user.dto'
 import userModel from '../user/user.model'
 import { UserService } from '../user/user.service'
+import { genSalt, hash } from 'bcrypt'
 
 export class AuthService {
 	constructor(
@@ -13,6 +14,7 @@ export class AuthService {
 
 	async registerUser(username: string, email: string, password: string) {
 		const role: Role = 'user'
+		password = await hash(password, await genSalt())
 
 		const jwt = this.tokenService.sign({
 			email,
