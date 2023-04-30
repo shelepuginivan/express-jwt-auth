57bac05b365884ce83513b72f517713dd314881c
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-30 19:04:31 +0300

    Added jwt token service

---
+++ src/token/token.service.ts
@@ -0,0 +1,23 @@
+import { JsonWebTokenError, sign, verify } from 'jsonwebtoken'
+
+import { Unauthorized } from '../exception/server.exception'
+
+export class TokenService {
+	sign(payload: object) {
+		return sign(payload, process.env.JWT_SECRET || '', {
+			expiresIn: process.env.JWT_EXPIRE_TIME || '30d'
+		})
+	}
+
+	verify(token: string) {
+		try {
+			return verify(token, process.env.JWT_SECRET || '')
+		} catch (error) {
+			if (error instanceof JsonWebTokenError) {
+				throw new Unauthorized(error.message)
+			}
+
+			throw error
+		}
+	}
+}
