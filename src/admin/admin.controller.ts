4433049a6cb3c6d22f0e76d00c7701830a7c90a1
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-30 20:00:57 +0300

    Added `allUsers` method

---
+++ src/admin/admin.controller.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express'
+import { AdminService } from './admin.service'
+
+export class AdminController {
+	constructor(private readonly adminService: AdminService) {}
+
+	async allUsers(req: Request, res: Response, next: NextFunction) {
+		try {
+			const jwt = req.headers.authorization?.split(' ')[1]
+			const users = await this.adminService.allUsers(jwt)
+
+			res.status(200).json(users)
+		} catch (error) {
+			next(error)
+		}
+	}
+}
