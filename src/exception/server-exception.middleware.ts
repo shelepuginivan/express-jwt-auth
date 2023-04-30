58d0cbc7c7937896bf9502578fd8ac32744e1a0e
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-30 18:38:23 +0300

    Fixed code styles (eslint)

--- src/exception/server-exception.middleware.ts
+++ src/exception/server-exception.middleware.ts
@@ -6,6 +6,6 @@ export const serverExceptionMiddleware: ErrorRequestHandler = (err: unknown, _re
 	if (err instanceof ServerException) {
 		res.status(err.status).json({ message: err.message })
 	} else {
-		res.status(500).json({message: 'unexpected server error'})
+		res.status(500).json({ message: 'unexpected server error' })
 	}
 }
