99fa830fac18553c95502c6ce3a52615780fc044
shelepuginivan <shelepuginivanm@gmail.com>
2023-04-30 19:06:19 +0300

    Added auth controller and `registerUser` method

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express'
+
+import { AuthService } from './auth.service'
+
+export class AuthController {
+	constructor(private readonly authService: AuthService) {}
+
+	async registerUser(req: Request, res: Response, next: NextFunction) {
+		try {
+			const { username, email, password } = req.body
+			const { jwt, user } = await this.authService.registerUser(username, email, password)
+
+			res.setHeader('Authorization', `Bearer ${jwt}`)
+			res.status(200).json(user)
+		} catch (error) {
+			next(error)
+		}
+	}
+}
