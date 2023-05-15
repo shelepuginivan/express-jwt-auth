44c2519f0bf711ebf0f12d6cbe33612835778c17
shelepuginivan <shelepuginivanm@gmail.com>
2023-05-15 20:59:54 +0300

    Fix serverExceptionMiddleware

--- src/exception/server-exception.middleware.ts
+++ src/exception/server-exception.middleware.ts
@@ -1,8 +1,13 @@
-import { ErrorRequestHandler, Request, Response } from 'express'
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
 
 import { ServerException } from './server.exception'
 
-export const serverExceptionMiddleware: ErrorRequestHandler = (err: unknown, _req: Request, res: Response) => {
+export const serverExceptionMiddleware: ErrorRequestHandler = (
+	err: unknown,
+	_req: Request,
+	res: Response,
+	_next: NextFunction
+) => {
 	if (err instanceof ServerException) {
 		res.status(err.status).json({ message: err.message })
 	} else {
